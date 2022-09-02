171008c4bacbc66d2a55d41599c5e76a46afb591
alajillo <alajillo2021@gmail.com>
2022-09-03 02:31:54 +0900

    Add : test result message

--- common/test.py
+++ common/test.py
@@ -1,7 +1,9 @@
 def check_solution(solution, test_case_list):
     for test_case in test_case_list:
         if solution(test_case["input"]) != test_case["output"]:
+            print('test is not passed :(')
             return False
+    print('test is passed!')
     return True
 
 
