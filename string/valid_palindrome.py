93e8d780daaf3558837bb0cff7b499a2391747ba
alajillo <alajillo2021@gmail.com>
2022-09-05 17:26:24 +0900

    Add : valid palidrome solution

--- string/valid_palindrome.py
+++ string/valid_palindrome.py
@@ -1,10 +1,43 @@
+import collections
+import re
+
 from common import check_solution
 
 TEST_CASE = [{'input': 'A man, a plan, a canal : Panama', 'output': True}]
 
 
-def test(value):
+def solution1(s: str) -> bool:
+    strs = []
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+    while len(strs) > 1:
+        if strs.pop(0) != strs.pop():
+            return False
     return True
 
 
-print(check_solution(test, TEST_CASE))
+check_solution(solution1, TEST_CASE)
+
+
+def solution2(s: str) -> bool:
+    strs = collections.deque()
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
+            return False
+    return True
+
+
+check_solution(solution2, TEST_CASE)
+
+
+def solution3(s: str) -> bool:
+    s = s.lower()
+    s = re.sub('[^a-z0-9]', '', s)
+    return s == s[::-1]
+
+
+check_solution(solution3, TEST_CASE)
