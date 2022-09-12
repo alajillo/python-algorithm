42bf73aad9c58e92ef55257540bb8da344e1f8a7
alajillo <alajillo2021@gmail.com>
2022-09-12 21:41:43 +0900

    Add : longest palindrome substring

---
+++ string/longest_palidrome_substring.py
@@ -0,0 +1,24 @@
+input = 'bbaaddaadwd'
+
+
+def solution(s):
+    def expand(left: int, right: int) -> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1: right]
+
+    if len(s) < 2 or s == s[::-1]:
+        return s
+
+    result = ''
+
+    for i in range(0, len(s) - 1):
+        result = max(result,
+                     expand(i, i + 1),
+                     expand(i, i + 2),
+                     key=len)
+
+    return result
+
+print(solution(input))
